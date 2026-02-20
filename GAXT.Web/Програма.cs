b8aa0d10bef9535c1c35f62c0bb2b3e6fe3bb9b5
Andrii Kurdiumov <kant2002@gmail.com>
2026-02-20 22:34:26 +0100

    Редактирование кода

--- GAXT.Web/Програма.cs
+++ GAXT.Web/Програма.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var построитель = WebAssemblyHostBuilder.CreateDefault(args);
-построитель.RootComponents.Add<App>("#app");
+построитель.RootComponents.Add<Апп>("#app");
 построитель.RootComponents.Add<HeadOutlet>("head::after");
 
 построитель.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(построитель.HostEnvironment.BaseAddress) });
